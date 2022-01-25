a03615005f6576803c3e6b01adb8577501fa0f82
AnneC <chretien.an@gmail.com>
2022-01-25 13:51:34 +0100

    working on front end

--- front_end/src/Components/Main.tsx
+++ front_end/src/Components/Main.tsx
@@ -1,21 +1,56 @@
 // this will show what we have in our account, staked and not
+
+/* eslint-disable spaced-comment */
+/// <reference types="react-scripts"/>
 import { useEthers} from "@usedapp/core"
 import helperConfig from "../helper-config.json"
+import networkMapping from "../chain-info/deployments/map.json"
+import {constants} from "ethers"
 import brownieConfig from "../brownie-config.json"
+import dapp from "../dapp.png"
+import weth from "../weth.png"
+import fau from "../fau.png"
+import { YourWallet } from "./yourWallet/YourWallet"
 
+export type Token = {
+    image: string
+    address: string
+    name: string
+}
 
 export const Main = () => {
     // this needs to :
-    // show token values from the wallet\
+    // show token values from the wallet
     // Get the address of different tokens
     // get the balance of the users wallet
     // we need the information stored on the brownie-config.yaml file
     // with addresses for tokens and networks
     const { chainId, error } = useEthers()
     const networkName = chainId ? helperConfig[chainId] : "dev"
-    console.log(typeof chainId)
-
-    console.log(networkName)
-    //const dappTokenAddress =?
-    return (<div>Hello!</div>)
+    // now we want to grab the address of the dappToken that we deployed on our kovan network 
+    const dappTokenAddress = chainId ? networkMapping[String(chainId)]["DappToken"][0]: constants.AddressZero
+    const wethTokenAddress = chainId ? brownieConfig["networks"][networkName]["weth_token"] : constants.AddressZero
+    const fauTokenAddress = chainId ? brownieConfig["networks"][networkName]["fau_token"] : constants.AddressZero
+    
+    // now we need to create something that displays the wallet : YourWallet
+    const supportedTokens: Array<Token> = [
+        {
+            image: dapp,
+            address: dappTokenAddress,
+            name: "DAPP"
+        },
+        {
+            image: weth,
+            address: wethTokenAddress,
+            name: "WETH"
+        },
+        {
+            image: fau,
+            address: fauTokenAddress,
+            name: "FAU"
+        },
+        
+    ]
+    // now we can pass the array of tokens to the Wallet
+    return (<YourWallet supportedTokens={supportedTokens}/>)
 }
---
+++ front_end/src/Components/yourWallet/YourWallet.tsx
@@ -0,0 +1,50 @@
+//get the token balances of the tokens we have
+import {Token} from "../Main"
+import React, {useState} from "react"
+import {Box, Tab} from "@material-ui/core"
+import {TabContext, TabList, TabPanel} from "@material-ui/lab"
+import {WalletBalance} from "./WalletBalance"
+
+interface YourWalletProps{
+    supportedTokens: Array<Token>
+}
+
+export const YourWallet = ({ supportedTokens}: YourWalletProps) =>{
+    // the first tab that we are going to use is going to be the first token we have
+    // for that we need to create a state hook
+    const [selectedTokenIndex, setSelectedTokenIndex] = useState<number>(0)
+    // this creates a variable called selectedTokenIndex and set it to whatever token we are on
+    // useState is a way to send states between renders of components
+    const handleChange = (event: React.ChangeEvent<{}>, newValue: string) => {
+        setSelectedTokenIndex(parseInt(newValue))
+    }
+    // handleChange allows us to select other tokens when selecting other tabs
+    return(
+        <Box>
+            <h1>I'm Your Wallet</h1>
+            <Box>
+                <TabContext value={selectedTokenIndex.toString()}>
+                    <TabList onChange={handleChange} aria-label="stake form tabs">
+                        {supportedTokens.map((token, index) => {
+                            return (
+                                <Tab label={token.name}
+                                    value={index.toString()}
+                                    key={index}/>
+                            )
+                        })}
+                    </TabList>
+                    {supportedTokens.map((token, index) => {
+                        return (
+                            <TabPanel value={index.toString()} key={index}>
+                                <div>
+                                    <WalletBalance token={supportedTokens[selectedTokenIndex]}/>
+                                    2. a big stake button
+                                </div>
+                            </TabPanel>
+                        )
+                    })}
+                </TabContext>
+            </Box>
+        </Box>
+    )
+}
