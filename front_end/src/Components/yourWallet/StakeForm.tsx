93bff16612a023fffc5d159aab6cc91efdd53092
AnneC <chretien.an@gmail.com>
2022-01-27 13:00:01 +0100

    problem with typescript

--- front_end/src/Components/yourWallet/StakeForm.tsx
+++ front_end/src/Components/yourWallet/StakeForm.tsx
@@ -1,8 +1,10 @@
 import { Token} from "../Main"
-import React, {useState} from "react"
-import {useEthers, useTokenBalance} from "@usedapp/core"
+import React, {useState, useEffect} from "react"
+import {useEthers, useTokenBalance, useNotifications} from "@usedapp/core"
 import { formatUnits } from "@ethersproject/units"
 import { Button, Input } from "@material-ui/core"
+import {useStakeTokens} from "../../hooks"
+import {utils} from "ethers"
 
 
 export interface StakeFormProps {
@@ -14,6 +16,7 @@ export const StakeForm = ({ token}: StakeFormProps) => {
     const {account} = useEthers()
     const tokenBalance = useTokenBalance(tokenAddress, account)
     const formattedTokenBalance: number = tokenBalance ? parseFloat(formatUnits(tokenBalance, 18)) :0
+    const {notifications} =useNotifications()
 
     const [amount, setAmount] = useState<number | string | Array<number | string>>(0)
     const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -21,6 +24,29 @@ export const StakeForm = ({ token}: StakeFormProps) => {
         setAmount(newAmount)
         console.log(newAmount)
     }
+
+    const {approveAndStake, approveErc20State} = useStakeTokens(tokenAddress)
+    const handleStakeSubmit = () => {
+        const amountAsWei = utils.parseEther(amount.toString())
+        return approveAndStake(amountAsWei.toString())
+    }
+    // lets watch the notifications
+    useEffect(() => {
+        if (notifications.filter(
+            (notification) =>
+                notification.type === "transactionSucceed" &&
+                notification.transactionName === "Approve ERC20 transfer").length > 0) {
+                    console.log("Approved!!")
+                }     
+        if (notifications.filter(
+            (notification) =>
+                notification.type === "transactionSucceed" &&
+                notification.transactionName === "Stake Tokens").length > 0) {
+                    console.log("Tokens Staked!!")
+                }
+    }, [notifications])
+
+
     // we want to return a button that will allow the user to select the number of tokens
     // they want to stake, need a form for that. input from materialui
     // created a handInputChange to track the number entered 
@@ -30,10 +56,11 @@ export const StakeForm = ({ token}: StakeFormProps) => {
         <div>
             <Input
                 onChange={handleInputChange}/>
-            <Button
-            color="primary"
-            size="large">
-                Stake your tokens!
+           <Button
+                onClick={handleStakeSubmit}
+                color="primary"
+                size="large">
+                Stake your Tokens!
             </Button>
         </div>
     )
--- front_end/src/hooks/useStakeTokens.ts
+++ front_end/src/hooks/useStakeTokens.ts
@@ -4,6 +4,7 @@ import TokenFarm from "../chain-info/contracts/TokenFarm.json"
 import ERC20 from "../chain-info/contracts/MockERC20.json"
 import { Contract } from "@ethersproject/contracts"
 import networkMapping from "../chain-info/deployments/map.json"
+import { useEffect, useState } from "react"
 
 export const useStakeTokens = (tokenAddress : string) => {
     // what we need to approve the staking :
@@ -14,12 +15,39 @@ export const useStakeTokens = (tokenAddress : string) => {
     const {abi} = TokenFarm
     const tokenFarmAddress = chainId ? networkMapping[String(chainId)]["TokenFarm"][0]: constants.AddressZero
     const tokenFarmInterface =new utils.Interface(abi)
-    const tokenFarmcontract = new Contract(tokenFarmAddress, tokenFarmInterface)
-
+    const tokenFarmContract = new Contract(tokenFarmAddress, tokenFarmInterface)
     const erc20ABI = ERC20.abi
     const erc20Interface = new utils.Interface(erc20ABI)
     const erc20Contract = new Contract(tokenAddress, erc20Interface)
     // approve
-    const {send: approveErc20Send, state: approveErc20State} = useContractFunction(erc20Contract, "approve", {transactionName: "Approve ERC20 Transfer"})
-    // now we can stake tokens
+    const { send: approveErc20Send, state: approveErc20State } =
+        useContractFunction(erc20Contract, "approve", {
+            transactionName: "Approve ERC20 transfer",
+        })
+    const approveAndStake = (amount: string) => {
+        setAmountToStake(amount)
+        return approveErc20Send(tokenFarmAddress, amount)
+    } // this function is gonna kick off the approve but also change the amount that we are going to stake
+
+    const [amountToStake, setAmountToStake] = useState("0")
+
+    const { send: stakeSend, state: stakeState} = 
+        useContractFunction(tokenFarmContract, "stakeTokens", {
+            transactionName: "Stake Tokens",
+        })
+
+    //uesEffect allows us top do something is some variable is changed
+    useEffect(() => {
+        if (approveErc20State.status === "Success") {
+            // stake function
+            stakeSend(amountToStake, tokenAddress)
+        }
+    }, [approveErc20State, amountToStake, tokenAddress])
+
+    //we add an array of anything we want to track, if anything in this array changes,
+    // we kick of the useEffect and do somrthing
+    // we want tot rack this approveErc20State
+
+    return {approveAndStake, approveErc20State}
+
 }
